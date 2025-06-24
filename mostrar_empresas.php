51b5d8b80c502ab88a1be8d9bb355527735060ed
Rubiel Quintero <rubielquintero0222@gmail.com>
2025-06-24 15:06:48 -0500

    actualizar repositorioRubiel0222

--- mostrar_empresas.php
+++ mostrar_empresas.php
@@ -7,35 +7,27 @@ $dbname = "inviza";
 // Crear conexión
 $conexion = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
+
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
 
-// Consulta a la base de datos
 $query = "SELECT * FROM empresa ORDER BY id_empresa ASC";
-$resultado = mysqli_query($conexion, $query);
+$resultado = $conexion->query($query);
 
-// Verificar si la consulta fue exitosa
-if (!$resultado) {
-    echo "<tr><td colspan='6'>Error en la consulta: " . mysqli_error($conexion) . "</td></tr>";
-    return;
-}
-
-// Mostrar los resultados
-if (mysqli_num_rows($resultado) > 0) {
-    while ($empresa = mysqli_fetch_assoc($resultado)) {
-        echo "<tr>";
-        echo "<td>{$empresa['id_empresa']}</td>";
-        echo "<td>{$empresa['nombre_empresa']}</td>";
-        echo "<td>{$empresa['nit']}</td>";
-        echo "<td>{$empresa['telefono_empresa']}</td>";
-        echo "<td>{$empresa['direccion_empresa']}</td>";
-        echo "<td>
-                <button class='editar' data-id='{$empresa['id_empresa']}'>Editar</button>
-                <button class='eliminar' data-id='{$empresa['id_empresa']}'>Eliminar</button>
-              </td>";
-        echo "</tr>";
+if ($resultado->num_rows > 0) {
+    while ($empresa = $resultado->fetch_assoc()) {
+        echo "<tr>
+                <td>{$empresa['id_empresa']}</td>
+                <td>{$empresa['nombre_empresa']}</td>
+                <td>{$empresa['nit']}</td>
+                <td>{$empresa['telefono_empresa']}</td>
+                <td>{$empresa['direccion_empresa']}</td>
+                <td>
+                  <button class='editar' data-id='{$empresa['id_empresa']}'>Editar</button>
+                  <button class='eliminar' data-id='{$empresa['id_empresa']}'>Eliminar</button>
+                </td>
+              </tr>";
     }
 } else {
     echo "<tr><td colspan='6'>No hay registros disponibles.</td></tr>";
